479a2547bb29da8fb95404f79de082cb48feab26
John Arnokouros <johnarn@windowslive.com>
2019-08-16 19:24:22 +0300

    Initial import file

---
+++ app/src/main/java/com/example/qrcode/import_qr_code_activity.java
@@ -0,0 +1,14 @@
+package com.example.qrcode;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class import_qr_code_activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.import_qr_code_activity);
+    }
+}
