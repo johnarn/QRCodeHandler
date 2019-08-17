48b1e66bcca52dceedba655f83fca5a505b35fc5
John Arnokouros <johnarn@windowslive.com>
2019-08-17 19:22:08 +0300

    Added comments to the code

--- app/src/main/java/com/example/qrcode/import_qr_code_activity.java
+++ app/src/main/java/com/example/qrcode/import_qr_code_activity.java
@@ -1,7 +1,5 @@
 package com.example.qrcode;
 
-import android.Manifest;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -14,12 +12,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import com.google.zxing.BinaryBitmap;
 import com.google.zxing.MultiFormatReader;
 import com.google.zxing.NotFoundException;
@@ -27,26 +21,50 @@ import com.google.zxing.RGBLuminanceSource;
 import com.google.zxing.Result;
 import com.google.zxing.common.HybridBinarizer;
 
+/**
+ *
+ *
+ * Import QR Code Activity
+ *
+ * Activity for importing existing QR Code from the gallery of the phone
+ *
+ *
+ */
 public class import_qr_code_activity extends AppCompatActivity {
+
+    /*
+     * Initialize views from the layout
+     */
     Button importButton;
     ImageView imageView;
     TextView textView;
+
     private int RESULT_LOAD_IMG = 1;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.import_qr_code_activity);
 
+        /*
+         * Find the views from the layout
+         */
+        importButton = findViewById(R.id.selectImageButton);
+        imageView = findViewById(R.id.iv);
+        textView = findViewById(R.id.txtView);
 
-        importButton = (Button) findViewById(R.id.selectImageButton);
-        imageView = (ImageView) findViewById(R.id.iv);
-        textView = (TextView) findViewById(R.id.txtView);
 
-
+        /*
+         * on Click Listener for the import Button
+         */
         importButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                /*
+                 * Start a new Intent to scan the storage for images
+                 */
                 Intent i = new Intent(
                         Intent.ACTION_PICK,
                         android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
@@ -64,66 +82,81 @@ public class import_qr_code_activity extends AppCompatActivity {
 
         if (resultCode == RESULT_OK) {
 
+            /*
+             * Find the URI of the image that the user has selected
+             */
             Uri selectedImage = data.getData();
             String[] filePathColumn = { MediaStore.Images.Media.DATA };
 
+            /*
+             * Create a cursor to access the image that the user has selected
+             */
             Cursor cursor = getContentResolver().query(selectedImage,
                     filePathColumn, null, null, null);
             cursor.moveToFirst();
 
+            /*
+             * Find the image absolute path inside the storage of the android phone
+             */
             int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
             String picturePath = cursor.getString(columnIndex);
+
+            /*
+             * Close the cursor
+             */
             cursor.close();
 
+            /*
+             * Set the QR Code that the user has selected at the imageView
+             */
             imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
 
+            /*
+             * Create a bitmap from the selected Image
+             */
             Bitmap bitmap = BitmapFactory.decodeFile(picturePath);
-            if (bitmap == null)
-            {
-            }
+
+            /*
+             * Get the width and height of the Image
+             */
             int width = bitmap.getWidth(), height = bitmap.getHeight();
+
+            /*
+             * Create a array with all the pixels of the Image
+             */
             int[] pixels = new int[width * height];
+
+            /*
+             * Get the pixels of the bitmap
+             */
             bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
-            bitmap.recycle();
-            bitmap = null;
+
+            /*
+             * Create a binary Bitmap and a MultiFormat reader to read the qr code
+             */
             RGBLuminanceSource source = new RGBLuminanceSource(width, height, pixels);
             BinaryBitmap bBitmap = new BinaryBitmap(new HybridBinarizer(source));
             MultiFormatReader reader = new MultiFormatReader();
-            try
-            {
+            try{
+                /*
+                 * Get the result of the binary bitmap to show to the user
+                 */
                 Result result = reader.decode(bBitmap);
+
+                /*
+                 * Set the text of the bitmap to the textView
+                 */
                 textView.setText(result.toString());
             }
-            catch (NotFoundException e)
-            {
-
+            catch (NotFoundException e){
+                e.printStackTrace();
             }
-
-
-
         }else {
+            /*
+             * If anything goes wrong inform the user
+             */
             Toast.makeText(import_qr_code_activity.this, "You haven't picked Image",Toast.LENGTH_LONG).show();
         }
     }
 
-
-    private void requestPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-            new AlertDialog.Builder(this).setTitle("Permission needed").setMessage("The permission is needed").setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    ActivityCompat.requestPermissions(import_qr_code_activity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
-
-                }
-            }).setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    dialogInterface.dismiss();
-                }
-            }).create().show();
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
-
-        }
-    }
 }
